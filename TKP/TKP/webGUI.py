52f16125db7100f03f450e8bcd4af7bd098cc7bf
ETO official <dynastxu@qq.com>
2024-11-23 21:02:43 +0800

    更改了样式

--- TKP/TKP/webGUI.py
+++ TKP/TKP/webGUI.py
@@ -36,7 +36,6 @@ def open_network():
         # 使用webbrowser打开临时HTML文件
         webbrowser.open(f'file://{tmp_file_name}')
 
-
 wordcloud_history = {"../docs/example.png":"example.png"}
 def update_wordcloud():
     # 获取词云图
@@ -74,34 +73,32 @@ with ui.tab_panels(tabs, value=wordcloud).classes('w-full h-full'):
                 with ui.card().classes('w-full h-full'):
                     ui.button("生成新的词云图", on_click=update_wordcloud)
                     img = ui.image().classes("w-full h-full").bind_source(history, "value")
-
-
             with splitter.after:
-                ui.label("源代码").style("size: 30px")
+                ui.label("源代码").style("font-size: 50px;")
                 with open("modules/wordcloud_generator.py", "r", encoding="utf-8") as f:
                     code = f.read()
                     ui.code(code).classes('w-full h-full')
     with ui.tab_panel(linechart):
         # 刘、关、张、曹操、孙权、周瑜在各回中出场次数变化的折线图
         with ui.card().classes('w-full h-full'):
-                    data = {
-                        "刘":appears.appears("玄德"),
-                        "关":appears.appears("关公"),
-                        "张":appears.appears("张飞"),
-                        "曹操":appears.appears("曹操"),
-                        "孙权":appears.appears("孙权"),
-                        "周瑜":appears.appears("周瑜")
-                    }
+            data = {
+                "刘":appears.appears("玄德"),
+                "关":appears.appears("关公"),
+                "张":appears.appears("张飞"),
+                "曹操":appears.appears("曹操"),
+                "孙权":appears.appears("孙权"),
+                "周瑜":appears.appears("周瑜")
+            }
 
-                    fig = go.Figure()
-                    for i in data.items():
-                        fig.add_trace(go.Scatter(x=[i for i in range(1, 121)], y=i[1], name=i[0]))
+            fig = go.Figure()
+            for i in data.items():
+                fig.add_trace(go.Scatter(x=[i for i in range(1, 121)], y=i[1], name=i[0], line=dict(shape='spline')))
 
-                    fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
-                    # 创建 Plotly 元素并显示
-                    ui.plotly(fig).classes('w-full h-40').style("width: 100%; height: 100%;")
+            fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+            # 创建 Plotly 元素并显示
+            ui.plotly(fig).classes("w-full h-full")
 
-        ui.label("源代码").style("size: 30px")
+        ui.label("源代码").style("font-size: 50px;")
         with open("modules/appears.py", "r", encoding="utf-8") as f:
             code = f.read()
             ui.code(code).classes('w-full h-full')
@@ -110,6 +107,8 @@ with ui.tab_panels(tabs, value=wordcloud).classes('w-full h-full'):
         with ui.row():
             ui.button("查看位置", on_click=open_map)
             ui.button("重新生成", on_click=draw_map)
+
+        ui.label("源代码").style("font-size: 50px;")
         with open("modules/address_analyze.py", "r", encoding="utf-8") as f:
             code = f.read()
             ui.code(code).classes('w-full h-full')
@@ -121,6 +120,7 @@ with ui.tab_panels(tabs, value=wordcloud).classes('w-full h-full'):
         with ui.row():
             ui.button("查看关系网", on_click=open_network)
             ui.button("重新生成", on_click=network_generator)
+        ui.label("源代码").style("font-size: 50px;")
         with open("modules/graphnode_generator.py", "r", encoding="utf-8") as f:
             code = f.read()
             ui.code(code).classes('w-full h-full')
